c3bd0e76e6eb89df663413b0ecc642dd8b5cf424
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-07 20:11:46 +0530

    iam_instance_profile

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,6 +26,9 @@ variable "zone_name" {
     default = "pka.in.net"
     type = string
 }
+variable "iam_instance_profile" {
+  default = "ec2-role-shell-script"
+}
 # this will comes from jenkins job through command line arguments
 variable "app_version" {
   
